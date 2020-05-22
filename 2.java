9a4b1e25d82ac69ad45c9c1518f7d31ee685ebc2
Vladisme <42497869+Vladisme@users.noreply.github.com>
2020-05-22 10:45:28 +0400

    Create 2.java

---
+++ 2.java
@@ -0,0 +1,48 @@
+import java.io.File;
+import java.io.IOException;
+import java.io.FileWriter;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+public class Main1 {
+    public static void main(String[] args) {
+        try {
+            File myObj = new File("filename.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("File created: " + myObj.getName());
+            } else {
+                System.out.println("File already exists.");
+                System.exit(0);
+            }
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter myWriter = new FileWriter("filename.txt");
+            Scanner in = new Scanner(System.in);
+            System.out.print("Write!: ");
+            String n = in.nextLine();
+            in.close();
+            myWriter.write(n);
+            myWriter.close();
+            System.out.println("Successfully wrote to the file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        try {
+            File myObj = new File("filename.txt");
+            Scanner myReader = new Scanner(myObj);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                System.out.println(data);
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+        System.gc();
+    }
+}
